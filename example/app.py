56f0d1e2224244b34a4afc5747e721e21bb2332b
Wilton Rodrigues <wiltonsr94@gmail.com>
2019-02-23 00:32:45 -0300

    Adding flash call from example

--- example/app.py
+++ example/app.py
@@ -8,6 +8,10 @@ app.config['SECRET_KEY'] = 'you-will-never-guess'
 @app.route('/')
 def index():
     flash("All OK")
+    flash("All OK", 'success')
+    flash("All Normal", 'info')
+    flash("Not So OK", 'error')
+    flash("So So", 'warning')
     return render_template('index.html')
 
 
