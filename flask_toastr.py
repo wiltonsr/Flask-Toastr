a8e8fe97b0023788504077deefc284ecb0db5714
Wilton Rodrigues <wiltonsr94@gmail.com>
2019-02-23 01:52:13 -0300

    Adding param support

--- flask_toastr.py
+++ flask_toastr.py
@@ -37,17 +37,32 @@ class _toastr(object):
 
     @staticmethod
     def message():
+        toastr_options = 'toastr.options.closeButton = %s; \
+        toastr.options.timeOut = %s; \
+        toastr.options.extendedTimeOut = %s; \
+        toastr.options.positionClass = \"%s\"; \
+        toastr.options.preventDuplicates = %s; \
+        toastr.options.newestOnTop = %s; \
+        toastr.options.progressBar = %s; ' % (
+            current_app.config.get('TOASTR_CLOSE_BUTTON'),
+            current_app.config.get('TOASTR_TIMEOUT'),
+            current_app.config.get('TOASTR_EXTENDED_TIMEOUT'),
+            current_app.config.get('TOASTR_POSITION_CLASS'),
+            current_app.config.get('TOASTR_PREVENT_DUPLICATES'),
+            current_app.config.get('TOASTR_NEWS_ON_TOP'),
+            current_app.config.get('TOASTR_PROGRESS_BAR'))
         message = Template('''
 {% with messages = get_flashed_messages(with_categories=true) %}
   {% if messages %}
     <script type="text/javascript">
       (function($) {
         $(document).ready(function() {
+          {{ toastr_options }}
           {% for category, message in messages|reverse %}
             {% if category is undefined or category == 'message' %}
-              toastr.info(\'{{ message }}\', \'{{ category }}\', {timeOut: 5000})
+              toastr.info(\'{{ message }}\', \'{{ category }}\')
             {% else %}
-              toastr.{{ category }}(\'{{ message }}\', \'{{ category }}\', {timeOut: 5000})
+              toastr.{{ category }}(\'{{ message }}\', \'{{ category }}\')
             {% endif %}
           {% endfor %}
         });
@@ -56,7 +71,9 @@ class _toastr(object):
   {% endif %}
 {% endwith %}
 ''')
-        return Markup(render_template(message, get_flashed_messages=get_flashed_messages))
+        return Markup(render_template(message,
+            get_flashed_messages=get_flashed_messages,
+            toastr_options=toastr_options))
 
 
 class Toastr(object):
@@ -69,6 +86,14 @@ class Toastr(object):
             app.extensions = {}
         app.extensions['toastr'] = _toastr
         app.context_processor(self.context_processor)
+
+        app.config.setdefault('TOASTR_CLOSE_BUTTON', 'true')
+        app.config.setdefault('TOASTR_TIMEOUT', 15000)
+        app.config.setdefault('TOASTR_EXTENDED_TIMEOUT', 1000)
+        app.config.setdefault('TOASTR_POSITION_CLASS', 'toast-top-right')
+        app.config.setdefault('TOASTR_PREVENT_DUPLICATES', 'false')
+        app.config.setdefault('TOASTR_NEWS_ON_TOP', 'false')
+        app.config.setdefault('TOASTR_PROGRESS_BAR', 'true')
 
     @staticmethod
     def context_processor():
