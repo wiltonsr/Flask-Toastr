5f5df1e6067cfe949aba0a3b6593333a1ea16fec
Wilton Rodrigues <wiltonsr94@gmail.com>
2021-10-31 19:11:15 -0300

    Add support for animation options
    - Closes #5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open('README.md', 'rt', encoding='utf8') as f:
 
 setup(
     name='Flask-Toastr',
-    version='0.5.5',
+    version='0.5.6',
     url='https://github.com/wiltonsr/Flask-Toastr/',
     license='MIT',
     author='Wilton Rodrigues',
